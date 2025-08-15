0f48b6215779366ca5170d8723b6d69f255959b3
Johannes Schindelin <johannes.schindelin@gmx.de>
2025-08-15 13:21:19 +0000

    CIHelper: add a convenient function to initialize e.g. the Git worktree
    In the upcoming GitHub Actions, we must assume that there is no
    persistent Git worktree to use, unlike the situation of GitGitGadget's
    Azure Pipelines, which use a single Git worktree that is retained and
    reused across Pipeline runs.
    
    To make this somewhat efficient, let's try to use a partial
    (semi-shallow) clone.
    
    And since there will be at least one GitHub workflow that runs several
    of GitGitGadget's GitHub Actions, let's play nice and reuse the
    initialized Git worktree.
    
    There are a couple of other things we want to set up in the upcoming
    GitHub Actions, e.g. handling the installation access tokens specified
    as GitHub Action inputs; Let's combine all of that setup in a single
    method of the `CIHelper` class.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- lib/ci-helper.ts
+++ lib/ci-helper.ts
@@ -1,6 +1,8 @@
+import * as core from "@actions/core";
 import * as fs from "fs";
 import * as util from "util";
 import addressparser from "nodemailer/lib/addressparser/index.js";
+import path from "path";
 import { ILintError, LintCommit } from "./commit-lint.js";
 import { commitExists, git, emptyTreeName } from "./git.js";
 import { GitNotes } from "./git-notes.js";
@@ -60,6 +62,54 @@ export class CIHelper {
         this.maxCommitsExceptions = this.config.lint?.maxCommitsIgnore || [];
         this.urlBase = `https://github.com/${this.config.repo.owner}/`;
         this.urlRepo = `${this.urlBase}${this.config.repo.name}/`;
+    }
+
+    public async setupGitHubAction(): Promise<void> {
+        // help dugite realize where `git` is...
+        process.env.LOCAL_GIT_DIRECTORY = "/usr/";
+        process.env.GIT_EXEC_PATH = "/usr/lib/git-core";
+
+        // get the access tokens via the inputs of the GitHub Action
+        this.setAccessToken("gitgitgadget", core.getInput("gitgitgadget-git-access-token"));
+        this.setAccessToken("git", core.getInput("git-git-access-token"));
+        this.setAccessToken("dscho", core.getInput("dscho-git-access-token"));
+
+        // eslint-disable-next-line security/detect-non-literal-fs-filename
+        if (!fs.existsSync(this.workDir)) await git(["init", "--bare", "--initial-branch", "main", this.workDir]);
+        for (const [key, value] of [
+            ["gc.auto", "0"],
+            ["remote.origin.url", `https://github.com/${this.config.repo.owner}/${this.config.repo.name}`],
+            ["remote.origin.promisor", "true"],
+            ["remote.origin.partialclonefilter", "blob:none"],
+        ]) {
+            await git(["config", key, value], { workDir: this.workDir });
+        }
+        console.time("fetch Git notes");
+        await git(
+            [
+                "-c",
+                "remote.origin.promisor=false", // let's fetch the notes with all of their blobs
+                "fetch",
+                "--no-tags",
+                "origin",
+                "--depth=1",
+                `+${GitNotes.defaultNotesRef}:${GitNotes.defaultNotesRef}`,
+            ],
+            {
+                workDir: this.workDir,
+            },
+        );
+        console.timeEnd("fetch Git notes");
+        this.gggNotesUpdated = true;
+        // "Unshallow" the refs by fetching the shallow commits with a tree-less filter.
+        // This is needed because Git will otherwise fall over left and right when trying
+        // to determine merge bases with really old branches.
+        const unshallow = async (workDir: string) => {
+            console.time(`Making ${workDir} non-shallow`);
+            console.log(await git(["fetch", "--filter=tree:0", "origin", "--unshallow"], { workDir }));
+            console.timeEnd(`Making ${workDir} non-shallow`);
+        };
+        await unshallow(this.workDir);
     }
 
     public setAccessToken(repositoryOwner: string, token: string): void {
