4239b01bd000bda33713ffdfa2d217e939dbc0e9
Johannes Schindelin <johannes.schindelin@gmx.de>
2025-08-16 23:28:55 +0000

    TEST: add the `is-allowed` Action
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

---
+++ is-allowed/index.js
@@ -0,0 +1,13 @@
+async function run() {
+  const { CIHelper } = await import("../dist/index.js")
+
+  const login = CIHelper.getActionsCore().getInput("login")
+
+  const ci = new CIHelper()
+  await ci.setupGitHubAction()
+  console.log(
+    `User '${login}' is ${ci.isAllowed(login) ? "" : "not "}allowed to use GitGitGadget.`,
+  )
+}
+
+run()
