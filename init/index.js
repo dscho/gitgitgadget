db2d0ee49ecd87b7fceb33cd3b61ed7115ee51f5
Johannes Schindelin <johannes.schindelin@gmx.de>
2025-08-16 09:35:33 +0000

    DEBUG: print some information about Git
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- init/index.js
+++ init/index.js
@@ -1,10 +1,29 @@
 async function run() {
   const { CIHelper } = await import("../dist/index.js")
+  const { execSync } = await import("child_process")
 
   const core = CIHelper.getActionsCore()
 
   // help dugite realize where `git` is...
   process.env.LOCAL_GIT_DIRECTORY = "/usr/"
+
+  console.log(`Git version: ${await CIHelper.git(["--version"])}`)
+  console.log(`Git exec path: ${await CIHelper.git(["--exec-path"])}`)
+  try {
+    console.log(execSync("ls -la /usr/lib/git-core 2>&1").toString("utf-8"))
+  } catch (e) {
+    console.error(e)
+  }
+  try {
+    console.log(
+      execSync("ldd /usr/lib/git-core/git-remote-https 2>&1").toString("utf-8"),
+    )
+  } catch (e) {
+    console.error(e)
+  }
+  console.log(
+    `Git remote-https -h: ${await CIHelper.git(["remote-https", "-h"])}`,
+  )
 
   const config = await CIHelper.getConfig()
   await CIHelper.initializeWorkDir("git", config)
