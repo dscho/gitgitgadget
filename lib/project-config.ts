98adf115d10411163198d56139ce90dd2631c7d7
Johannes Schindelin <johannes.schindelin@gmx.de>
2025-09-08 18:59:22 +0200

    IConfig: remove no longer needed `project.branch`
    Ever since 6d397b0f (Remove support for the CLI mode, 2023-12-31), there
    is no way the `ProjectOptions` are constructed without a `baseCommit`.
    Therefore it is always provided.
    
    And since `baseCommit` is always provided, the `ProjectOptions.get()`
    function no longer needs `upstreamBranch`: It only used it in a "is this
    rebased?" check that apparently hasn't been called anymore, ever.
    
    So that leaves just one last user of that `upstreamBranch` attribute,
    but that's a test that wants to mocj a `prMeta` from a `ProjectOptions`
    instance, something that only the CLI mode used to do, but no remaining
    production code. That test can easily replace that
    `options2.upstreamBranch` with a sensible, hard-coded string.
    
    And since that's gone, we can also remove the `project.branch` attribute
    because nothing uses it anymore.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- lib/project-config.ts
+++ lib/project-config.ts
@@ -2,7 +2,6 @@ import { ILintCommitConfig } from "./commit-lint.js";
 
 export type projectInfo = {
     to: string; // email to send patches to
-    branch: string; // upstream branch a PR must be based on
     cc: string[]; // emails to always be copied on patches
     urlPrefix: string; // url to 'listserv' of mail (should it be in mailrepo?)
 };
